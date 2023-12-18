4e5661a81aec7be45d5dc6adde50530fe8825541
andreafrancioni <andreafrancioni.work@gmail.com>
2023-12-18 15:50:35 +0100

    Pusha esercizio badword

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BadWords</title>
+    <?php
+    $name = "Andrea"
+    ?>
+</head>
+<body>
+    <h1>
+        Benvenuto, <?php echo $name ?>
+    </h1>
+</body>
+</html>
